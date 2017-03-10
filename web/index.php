ddc591e81cc1b7fd1e78b0004c164e754cbcf9d7
Manuel Reinhard <manu@sprain.ch>
2017-03-10 21:08:21 +0100

    Removed unneeded use statements

--- web/index.php
+++ web/index.php
@@ -1,9 +1,5 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../app/skeleton/bootstrap.php';
 
